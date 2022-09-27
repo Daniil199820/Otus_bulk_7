14ab29c3daa219a7e28c0f85393c05b2c150f998
Daniil <dry.ginpost@gmail.com>
2022-09-28 00:01:52 +0300

    add state

--- Model.h
+++ Model.h
@@ -111,3 +111,78 @@ public:
     }
 };
 
+/*
+  class LoginHandler : public ICommandHandler {
+    public:
+        void login(Application *) override {
+            std::cout << "already login" << std::endl;
+        }
+        void do_it(Application *m) override {
+            std::cout << "success" << std::endl;
+            /*std::cout << "start work" << std::endl;
+            m->set_current(ICommandHandlerPtr{new InProgressHandler()});*/
+        }
+        void logout(Application *m) override;
+    };
+
+    class AnonymousHandler : public ICommandHandler {
+    public:
+        void login(Application *m) override {
+            std::cout << "login success" << std::endl;
+            m->set_current(ICommandHandlerPtr{new LoginHandler()});  // Чтобы переключить состояние, нужно подать другой объект-состояние в контекст.
+        }
+        void do_it(Application *) override {
+            std::cout << "anonymous error" << std::endl;
+        }
+        void logout(Application *m) override {
+            std::cout << "already logout" << std::endl;
+        }
+    };
+
+    /*class InProgressHandler : public ICommandHandler {
+    public:
+        void login(Application *) override {
+            std::cout << "already login" << std::endl;
+        }
+        void do_it(Application *m) override {
+            std::cout << "stop work" << std::endl;
+            m->set_current(ICommandHandlerPtr{new LoginHandler()});
+        }
+        void logout(Application *) override {
+            std::cout << "error: stop working first" << std::endl;
+        }
+    };*/
+
+    void LoginHandler::logout(Application *m) {
+        /*
+        Состояние получает указатель на объект контекста. 
+        Через него не только можно получать из контекста нужную информацию, но и осуществлять смену его состояния.
+        */
+        std::cout << "work as anonymous" << std::endl;
+        m->set_current(ICommandHandlerPtr{new AnonymousHandler()});
+    }
+
+    Application::Application() {
+        m_handler = ICommandHandlerPtr{new AnonymousHandler()};
+        std::cout << std::endl;
+    }
+
+    // Переходы сложно отследить!
+
+    void example() {
+        std::cout << "pattern::example" << std::endl;
+
+        Application app;
+
+        app.do_it();
+        app.logout();
+        app.login();
+        app.do_it();
+        app.login();
+        app.logout();
+
+        std::cout << std::endl << std::endl;
+    }
+}
+
+*/
